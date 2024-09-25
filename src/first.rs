6f6e661e85dc06a43ca8262b14970169b2a29322
Rohit Kumar Bindal <kumarrohitbindal@gmail.com>
2024-09-25 16:28:06 +0530

    First list: pop from the beginning of the list.

--- src/first.rs
+++ src/first.rs
@@ -18,7 +18,7 @@ impl List {
     pub fn new() -> Self {
         List { head: Link::Empty }
     }
-    
+
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem,
@@ -26,6 +26,21 @@ impl List {
             next: mem::replace(&mut self.head, Link::Empty),
         });
         self.head = Link::More(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        /*
+        Replacing head with an empty link so that we can replace the head
+        in order to remove an element.
+        */
+        match mem::replace(&mut self.head, Link::Empty) {
+            Link::Empty => None,
+            Link::More(node) => {
+                // Replacing the head, i.e, removing the element.
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
     }
 }
 
