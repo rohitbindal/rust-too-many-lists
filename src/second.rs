942520277cac3906cc7e493a082293370ba2ada4
Rohit Kumar Bindal <kumarrohitbindal@gmail.com>
2024-10-04 14:52:19 +0530

    Using 'Option' for Link

--- src/second.rs
+++ src/second.rs
@@ -1,4 +1,4 @@
-use std::mem
+use std::{io::Lines, mem};
 
 pub struct List {
     head: Link,
@@ -6,8 +6,40 @@ pub struct List {
 
 type Link = Option<Box<Node>>;
 
-struct  Node {
+struct Node {
     elem: i32,
     next: Link,
 }
 
+impl List {
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    pub fn push(&mut self, elem: i32) {
+        let new_node = Box::new(Node {
+            elem,
+            next: mem::replace(&mut self.head, None),
+        });
+        self.head = Some(new_node);
+    }
+
+    pub fn pop(&mut self) -> Option<i32> {
+        match mem::replace(&mut self.head, None) {
+            None => None,
+            Some(node) => {
+                self.head = node.next;
+                Some(node.elem)
+            }
+        }
+    }
+}
+
+impl Drop for List {
+    fn drop(&mut self) {
+        let mut current_link = mem::replace(&mut self.head, None);
+        while let Some(mut boxed_node) = current_link {
+            current_link = mem::replace(&mut boxed_node.next, None);
+        }
+    }
+}
