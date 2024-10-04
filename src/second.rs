dfc891ee9b1fa17cf32ee3716391119238778d19
Rohit Kumar Bindal <kumarrohitbindal@gmail.com>
2024-10-04 15:00:02 +0530

    Added tests

--- src/second.rs
+++ src/second.rs
@@ -1,5 +1,3 @@
-use std::{io::Lines, mem};
-
 pub struct List {
     head: Link,
 }
@@ -19,27 +17,48 @@ impl List {
     pub fn push(&mut self, elem: i32) {
         let new_node = Box::new(Node {
             elem,
-            next: mem::replace(&mut self.head, None),
+            next: self.head.take(),
         });
         self.head = Some(new_node);
     }
 
     pub fn pop(&mut self) -> Option<i32> {
-        match mem::replace(&mut self.head, None) {
-            None => None,
-            Some(node) => {
-                self.head = node.next;
-                Some(node.elem)
-            }
-        }
+        self.head.take().map(|node| {
+            self.head = node.next;
+            node.elem
+        })
     }
 }
 
 impl Drop for List {
     fn drop(&mut self) {
-        let mut current_link = mem::replace(&mut self.head, None);
+        let mut current_link = self.head.take();
         while let Some(mut boxed_node) = current_link {
-            current_link = mem::replace(&mut boxed_node.next, None);
+            current_link = boxed_node.next.take();
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics() {
+        let mut list = List::new();
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
     }
 }
