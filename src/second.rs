8bc097c3036d52667357230459b8294d4ef32bdf
Rohit Kumar Bindal <kumarrohitbindal@gmail.com>
2024-10-09 16:45:40 +0530

    fixed formatting

--- src/second.rs
+++ src/second.rs
@@ -3,7 +3,7 @@ pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
 }
 pub struct IterMut<'a, T> {
-    next: Option<&'a mut Node<T>>
+    next: Option<&'a mut Node<T>>,
 }
 
 pub struct List<T> {
@@ -54,7 +54,7 @@ impl<T> List<T> {
             next: self.head.as_deref(),
         }
     }
-    
+
     pub fn iter_mut(&mut self) -> IterMut<'_, T> {
         IterMut {
             next: self.head.as_deref_mut(),
@@ -62,7 +62,7 @@ impl<T> List<T> {
     }
 }
 
-impl <'a, T> Iterator for IterMut<'a, T> {
+impl<'a, T> Iterator for IterMut<'a, T> {
     type Item = &'a mut T;
     fn next(&mut self) -> Option<Self::Item> {
         self.next.take().map(|node| {
@@ -169,16 +169,17 @@ mod test {
         assert_eq!(iter.next(), Some(&2));
         assert_eq!(iter.next(), Some(&1));
     }
-    
+
     #[test]
     fn iter_mut() {
         let mut list = List::new();
-        list.push(1); list.push(2); list.push(3);
-    
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
         let mut iter = list.iter_mut();
         assert_eq!(iter.next(), Some(&mut 3));
         assert_eq!(iter.next(), Some(&mut 2));
         assert_eq!(iter.next(), Some(&mut 1));
     }
-
 }
